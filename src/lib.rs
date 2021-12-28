43c912e745586a8b2fb633be91add5f698e07ea3
Peter Allin <peter@peca.dk>
2021-12-28 16:13:14 +0100

    Add set 1 challenge 7 solution

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,4 @@ pub mod s1c3;
 pub mod s1c4;
 pub mod s1c5;
 pub mod s1c6;
+pub mod s1c7;
---
+++ src/s1c7.rs
@@ -0,0 +1,32 @@
+use crate::data::{Ciphertext, Key, Plaintext};
+use aes::Aes128;
+use anyhow::Result;
+use block_modes::block_padding::ZeroPadding;
+use block_modes::{BlockMode, Ecb};
+type Aes128Ecb = Ecb<Aes128, ZeroPadding>;
+
+pub fn aes128_ecb_decrypt(ciphertext: &Ciphertext, key: &Key) -> Result<Plaintext> {
+    let cipher = Aes128Ecb::new_from_slices(&key.0, &vec![])?;
+    let plaintext = cipher.decrypt_vec(&ciphertext.0)?;
+    Ok(Plaintext(plaintext))
+}
+
+#[cfg(test)]
+mod test {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn test_aes128_ecb_decrypt() {
+        let ciphertext: String = include_str!("7.txt")
+            .chars()
+            .filter(|c| *c != '\n')
+            .collect();
+        let ciphertext = Ciphertext(base64::decode(ciphertext).unwrap());
+        let key = Key::from_str("YELLOW SUBMARINE").unwrap();
+        let plaintext = aes128_ecb_decrypt(&ciphertext, &key).unwrap();
+        let plaintext = String::from_utf8_lossy(&plaintext.0);
+        assert!(plaintext.contains("Play that funky music"));
+    }
+}
